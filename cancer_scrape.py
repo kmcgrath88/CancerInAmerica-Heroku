383b4087dc4ed1a48ef9380ac4b723cc6663ae58
Kristi McGrath <mcgrathkristi@gmail.com>
2020-10-15 21:38:06 -0400

    Updates

--- cancer_scrape.py
+++ cancer_scrape.py
@@ -21,8 +21,12 @@ def init_browser():
 
     chrome_options = Options()
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+
+    chrome_options.add_argument('--disable-gpu')
+    
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     return driver.get("https://www.cancer.gov/news-events")
     # return Browser("chrome", **executable_path, headless=False)
