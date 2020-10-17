540bd067911101349481e3b33f1b000ae79f2870
Kristi McGrath <mcgrathkristi@gmail.com>
2020-10-17 09:49:34 -0400

    Updates

--- cancer_scrape.py
+++ cancer_scrape.py
@@ -6,11 +6,21 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import os
+import requests
 
 browser = None
 # Function to intialize brower
-def init_browser():
+# def init_browser():
+#     # browser = driver.get("https://www.cancer.gov/news-events")
 
+#     executable_path = {"executable_path": os.environ.get("CHROMEDRIVER_PATH")}
+#     browser = Browser("chrome", **executable_path, headless=False)
+
+    # return Browser("chrome", **executable_path, headless=False)
+
+# Function to scrape information - steps from jupyter notebook
+def scrape():
+    #init_browser()
     # executable_path = {"executable_path": "chromedriver.exe"}
     # chrome_options = webdriver.ChromeOptions()
     # chrome_options.add_argument("--headless")
@@ -18,44 +28,27 @@ def init_browser():
     # chrome_options.add_argument("--no-sandbox")
     # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     # driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-    # return driver.get("https://www.cancer.gov/news-events")
+    # driver.get("https://www.cancer.gov/news-events")
 
-    # chrome_options = Options()
-    # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    # chrome_options.add_argument('--headless')
-    # chrome_options.add_argument('--no-sandbox')
-    # chrome_options.add_argument('--disable-dev-shm-usage')
-
-    # chrome_options.add_argument('--disable-gpu')
     
-    # # driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-
-
-    # browser = driver.get("https://www.cancer.gov/news-events")
-
-    executable_path = {"executable_path": os.environ.get("CHROMEDRIVER_PATH")}
-    browser = Browser("chrome", **executable_path, headless=False)
-
-    # return Browser("chrome", **executable_path, headless=False)
-
-# Function to scrape information - steps from jupyter notebook
-def scrape():
-    init_browser()
+    # driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
     #####--Cancer News Site--#####
     url = "https://www.cancer.gov/news-events"
-    browser.visit(url)
+    # browser.visit(url)
+    resp = requests.get(url)
 
     # Allow page to load
     time.sleep(1) 
 
     # Scrape page into Soup
-    html = browser.html
-    soup = bs(html, "html.parser")
+    # html = browser.html
+    soup = bs(resp.text, "html.parser")
 
     # Create empty dictionary
     news_data = {}
     # Retrieving the latest news title and text, and adding to dictionary
+    # article = soup.find_element_by_class_name(class_name='feature-card')
     article = soup.find('div', class_= 'feature-card')
     news_data['article_title'] = article.find('h3').text
     news_data['article_text'] = article.find('p').text
@@ -63,12 +56,13 @@ def scrape():
     news_data['news_url'] = "https://www.cancer.gov" + article.find('a')['href']
 
     # Click first news article
-    browser.find_by_css('div.image-hover').click()
+    resp2 = requests.get(news_data['news_url'])
+    soup2 = bs(resp2.text, "html.parser")
     # Retrieving date and adding to dictionary
-    news_data['date'] = browser.find_by_css('time').text
+    news_data['date'] = soup2.select('time').text
 
     # Quit the browser after scraping
-    browser.quit()
+    # browser.quit()
 
     # Return results
     return news_data
